677b456d5c95df687e5be4f8192187be56f10971
Beartoe7 <64451243+Beartoe7@users.noreply.github.com>
2023-03-06 07:56:37 +0100

    ok

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,26 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Action";
+
+function Tweet(props) {
+  const { tweet } = props;
+  return (
+    <div className="tweet">
+       <ProfileImage image={props.tweet.user.image} />
+
+<div className="body">
+  <div className="top">
+  <User userData={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+        <Message message={props.tweet.message} />
+        <Actions />
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+export default Tweet;
